ca1b5e0de044cfdfc2be05ee758b108fc51a203d
sabil <sabil.danish1997@gmail.com>
2023-05-24 21:59:00 +0530

    some new library added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res) => {
+    res.send("Hello World")
+})
 
 
 
 
 const PORT = process.env.PORT || 5001
+
+app.listen(PORT, console.log(`port running on port number ${PORT}`))
